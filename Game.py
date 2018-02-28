abe8f8f4b20cf6ff4283cc051255a10d4dadf6c4
Samuel Freeman <samfreeman8or@gmail.com>
2018-02-28 12:17:49 +0000

    Completed GameObject class, and tested it in game

--- Game.py
+++ Game.py
@@ -1,16 +1,21 @@
 import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 from State import State
 from Vector import Vector
+from GameObject import GameObject
 
 CANVAS_WIDTH = 800
 CANVAS_HEIGHT = 450
 state = State()
 
+# Testing game object
+gameob = GameObject(Vector((100, 100)), Vector((2, 0)), [20, 50], 100)
+
 def draw(canvas):
     update()
+    gameob.draw(canvas)
 
 def update():
-    pass
+    gameob.update()
 
 frame = simplegui.create_frame("Game Name Goes Here", CANVAS_WIDTH, CANVAS_HEIGHT, 0)
 frame.set_draw_handler(draw)
