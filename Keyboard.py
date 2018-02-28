ac75e8d3da08e3e29038c685254a084b3a0e857c
domlobo <dk00685@gmail.com>
2018-02-28 11:36:18 +0000

    keyup method

--- Keyboard.py
+++ Keyboard.py
@@ -2,7 +2,7 @@ try:
     import simplegui
 except ImportError :
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
-    
+
 class Keyboard:
 
     def __init__(self):
@@ -12,5 +12,21 @@ class Keyboard:
         self.down=False
 
     def keyDown(self, key):
-        if key ==:
-            pass
+        if key ==simplegui.KEY_MAP['right']:
+            self.right = True
+        elif key == simplegui.KEY_MAP['left']:
+            self.left = True
+        elif key == simplegui.KEY_MAP['up'] or key == simplegui.KEY_MAP['space']:
+            self.up = True
+        elif key == simplegui.KEY_MAP['down']:
+            self.down = True
+
+    def keyUp(self, key):
+        if key ==simplegui.KEY_MAP['right']:
+            self.right = False
+        elif key == simplegui.KEY_MAP['left']:
+            self.left = False
+        elif key == simplegui.KEY_MAP['up'] or key == simplegui.KEY_MAP['space']:
+            self.up = False
+        elif key == simplegui.KEY_MAP['down']:
+            self.down = False
