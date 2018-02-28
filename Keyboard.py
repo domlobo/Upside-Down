573b8db1ce5aa2bf5914f9721b65c6e403e70f3f
domlobo <dk00685@gmail.com>
2018-02-28 11:34:40 +0000

    keydown method

---
+++ Keyboard.py
@@ -0,0 +1,16 @@
+try:
+    import simplegui
+except ImportError :
+    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+    
+class Keyboard:
+
+    def __init__(self):
+        self.left =False
+        self.right=False
+        self.up=False
+        self.down=False
+
+    def keyDown(self, key):
+        if key ==:
+            pass
