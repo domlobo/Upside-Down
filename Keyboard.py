74c87cdbe846a78c1c76fd98b9a0ea486a5c5dc0
domlobo <dk00685@gmail.com>
2018-02-28 12:09:58 +0000

    adding wasd keys

--- Keyboard.py
+++ Keyboard.py
@@ -13,13 +13,13 @@ class Keyboard:
 
     #handling key press down
     def keyDown(self, key):
-        if (key ==simplegui.KEY_MAP['right']):
+        if (key == simplegui.KEY_MAP['right']) or (key == simplegui.KEY_MAP['d']):
             self.right = True
-        elif (key == simplegui.KEY_MAP['left']):
+        elif (key == simplegui.KEY_MAP['left']) or (key == simplegui.KEY_MAP['a']):
             self.left = True
-        elif (key == simplegui.KEY_MAP['up']) or (key == simplegui.KEY_MAP['space']):
+        elif (key == simplegui.KEY_MAP['up']) or (key == simplegui.KEY_MAP['space']) or (key == simplegui.KEY_MAP['w']):
             self.up = True
-        elif (key == simplegui.KEY_MAP['down']):
+        elif (key == simplegui.KEY_MAP['down']) or (key == simplegui.KEY_MAP['s']):
             self.down = True
 
     #handling key release
