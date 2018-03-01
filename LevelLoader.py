d359a7bbbb71505e819a51963488893a802bc305
domlobo <dk00685@gmail.com>
2018-03-01 21:49:32 +0000

    adding tutorial layer 1

--- LevelLoader.py
+++ LevelLoader.py
@@ -9,9 +9,10 @@ class LevelLoader:
     def __init__(self,player,inter):
         self.player = player
         self.inter = inter
-        self.levels = (Level('https://raw.githubusercontent.com/domlobo/CS1830-Games-Lab/master/images/background/mario/hills.png?token=APgKaX34fYs3AdO_hIuWXG7_M_m2FNK7ks5aoZucwA%3D%3D','https://raw.githubusercontent.com/domlobo/CS1830-Games-Lab/master/images/background/mario/Mario-world-1.1.png?token=APgKaRDpvbFC1-I2_-idKugD_sm8BXH-ks5aoaAswA%3D%3D',
+        tutorialOne = Level('https://raw.githubusercontent.com/domlobo/CS1830-Games-Lab/master/images/background/link/link-tutorial-world.jpg?token=APgKaYNg0cYs3Pz7tg6Yk1TWGxuLV6Egks5aoa9MwA%3D%3D','','https://raw.githubusercontent.com/domlobo/CS1830-Games-Lab/master/images/background/mario/Mario-world-clouds.png?token=APgKaSIkI0DepOY41ozT1p6k03q-8_-vks5aoT75wA%3D%3D',player,inter)
+        self.levels = (tutorialOne,Level('https://raw.githubusercontent.com/domlobo/CS1830-Games-Lab/master/images/background/mario/hills.png?token=APgKaX34fYs3AdO_hIuWXG7_M_m2FNK7ks5aoZucwA%3D%3D','https://raw.githubusercontent.com/domlobo/CS1830-Games-Lab/master/images/background/mario/Mario-world-1.1.png?token=APgKaRDpvbFC1-I2_-idKugD_sm8BXH-ks5aoaAswA%3D%3D',
         'https://raw.githubusercontent.com/domlobo/CS1830-Games-Lab/master/images/background/mario/Mario-world-clouds.png?token=APgKaSIkI0DepOY41ozT1p6k03q-8_-vks5aoT75wA%3D%3D',
-         player,inter),Level("","","",player,inter))
+         player,inter))
         self.levelCounter=-1
         self.currentLevel=self.levels[self.levelCounter]
 
