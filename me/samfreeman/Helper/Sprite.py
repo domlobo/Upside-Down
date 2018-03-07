02ff3d8c6edd9fa09245a63bee07747a03968fc2
domlobo <dk00685@gmail.com>
2018-03-07 02:49:21 +0000

    fixing the height/width error

--- me/samfreeman/Helper/Sprite.py
+++ me/samfreeman/Helper/Sprite.py
@@ -2,8 +2,8 @@
 import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 from me.samfreeman.Helper.Clock import Clock
 
-
 class Sprite:
+
     def __init__(self, assetPath, isSpriteSheet = False, rows=1, cols=1):
         if assetPath != "":
             self.image = simplegui._load_local_image(assetPath)
@@ -56,8 +56,8 @@ class Sprite:
     def draw(self, position, canvas, width=0, height=0):
         # Target width and height
         # If no width or height is specified, it will display the full size of the image
-        d_width = self.image.get_width()  # Destination width
-        d_height = self.image.get_height()  # Destination height
+        d_width = self.frameWidth # Destination width
+        d_height = self.frameHeight  # Destination height
         if width > 0: d_width = width
         if height > 0: d_height = height
 
@@ -71,9 +71,8 @@ class Sprite:
 
         canvas.draw_image(
             self.image,
-            (self.frameWidth*self.frameIndex[0]+self.frameCentre[0],
-                self.frameHeight*self.frameIndex[1]+self.frameCentre[1]),
-            (self.frameWidth, self.frameWidth),
+            ((self.frameWidth*self.frameIndex[0])+self.frameCentre[0], (self.frameHeight*self.frameIndex[1])+self.frameCentre[1]),
+            (self.frameWidth, self.frameHeight),
             position.getP(),
             (d_width, d_height)
         )
