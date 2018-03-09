6662159cdb6fd46ce9a81ecb4c27df1f6af55284
domlobo <dk00685@gmail.com>
2018-03-09 00:52:49 +0000

    adding conditions for the jumping collisions

--- me/samfreeman/Input/Interaction.py
+++ me/samfreeman/Input/Interaction.py
@@ -65,7 +65,7 @@ class Interaction:
         entity.canMoveDown = True
         for currentObject in objects[:]:
             if currentObject.boundingBox.overlaps(entity.boundingBox):
-                if entity.boundingBox.bottom > currentObject.boundingBox.top and(entity.position.x <= currentObject.boundingBox.right and entity.position.x >= currentObject.boundingBox.left):
+                if entity.boundingBox.bottom > currentObject.boundingBox.top and(entity.position.x <= currentObject.boundingBox.right and entity.position.x >= currentObject.boundingBox.left and entity.position.y <= currentObject.boundingBox.top):
                     entity.canMoveDown = False
                 if (entity.boundingBox.right > currentObject.boundingBox.left) and (entity.position.x < currentObject.position.x) and (entity.position.y <= currentObject.boundingBox.bottom and entity.position.y >= currentObject.boundingBox.top):
                     entity.canMoveRight = False
@@ -73,5 +73,5 @@ class Interaction:
                 if (entity.boundingBox.left < currentObject.boundingBox.right) and (entity.position.x > currentObject.position.x) and (entity.position.y <= currentObject.boundingBox.bottom and entity.position.y >= currentObject.boundingBox.top):
                     entity.canMoveLeft = False
                     entity.velocity.x *= -1
-                if entity.boundingBox.top < currentObject.boundingBox.bottom and(entity.position.x <= currentObject.boundingBox.right and entity.position.x >= currentObject.boundingBox.left):
+                if entity.boundingBox.top < currentObject.boundingBox.bottom and(entity.position.x <= currentObject.boundingBox.right and entity.position.x >= currentObject.boundingBox.left and entity.position.y >= currentObject.boundingBox.bottom):
                     entity.canMoveUp = False
