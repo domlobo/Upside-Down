ffd83e0c1676922d9c982f9e110e2870871b6953
domlobo <dk00685@gmail.com>
2018-03-10 15:48:37 +0000

    adding the correct level loading sequence

--- me/samfreeman/GameControl/LevelLoader.py
+++ me/samfreeman/GameControl/LevelLoader.py
@@ -38,8 +38,8 @@ class LevelLoader:
                         "images/background/doom/second layer.png",
                          "", player,inter,"Doom-1")
         # Creating list of levels
-        self.levels =(doomOne,)#(tutorialOne,tutorialTwo,tutorialThree,marioOne,doomOne)
-        self.enemyFiles =("enemies/doomOne.txt",)#("enemies/tutorialOne.txt","enemies/tutorialTwo.txt","enemies/tutorialThree.txt","enemies/marioOne.txt","enemies/doomOne.txt")
+        self.levels =(tutorialOne,tutorialTwo,tutorialThree,marioOne,doomOne)
+        self.enemyFiles =("enemies/tutorialOne.txt","enemies/tutorialTwo.txt","enemies/tutorialThree.txt","enemies/marioOne.txt","enemies/doomOne.txt")
         # Selecting the first level
         self.levelCounter=0
         self.currentLevel=self.levels[self.levelCounter]
