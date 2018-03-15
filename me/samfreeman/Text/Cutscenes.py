2a51a8c5c20fc6ae5593ef35cfe73a3601fe8db7
Samuel Freeman <samfreeman8or@gmail.com>
2018-03-15 16:38:45 +0000

    Updated the cutscene to only load each sprite once

--- me/samfreeman/Text/Cutscenes.py
+++ me/samfreeman/Text/Cutscenes.py
@@ -6,44 +6,46 @@ from me.samfreeman.Helper.Sprite import Sprite
 class AllCutscenes:
     def __init__(self, frame):
         self.all = [Cutscene(frame) for i in range(6)]
-
+        self.playerSprite = Sprite("images/cutscenes/Player_PhoneCall_Right.png", 1, 7, True)
+        self.wifeSprite = Sprite("images/cutscenes/Wife_PhoneCall_Left.png", 1, 5, True)
+        self.linkSprite = Sprite("images/interactive-sprites/link/link-walk-left.png", 1, 4, True)
         # First
 
         self.all[0].setTitle("Part 1: The Beginning")
         self.all[0].addText("Bob Froman",
                              "This is a test to see if the whole system works, I'm really hoping that it does",
-                             Sprite("images/cutscenes/Player_PhoneCall_Right.png", 1, 7, True),
+                             self.playerSprite,
                              "Froman", "Yes this test works well, and it is quite cool",
-                             Sprite("images/cutscenes/Wife_PhoneCall_Left.png", 1, 5, True))
+                             self.wifeSprite)
         self.all[0].addText("Bob Froman", "Thanks for your input,  Bob, it was helpful",
-                             Sprite("images/cutscenes/Player_PhoneCall_Right.png", 1, 7, True),
-                             "Froman", "Fuck off", Sprite("images/cutscenes/Wife_PhoneCall_Left.png", 1, 5, True))
+                             self.playerSprite,
+                             "Froman", "Fuck off", self.wifeSprite)
         self.all[0].addText("Bob Froman", "This is the last test to test my function",
-                             Sprite("images/cutscenes/Player_PhoneCall_Right.png", 1, 7, True))
+                             self.playerSprite)
         # IF YOU WANT THE SPEAKER TO GO FROM SINGLE TO DOUBLE, REPEAT THE TEXT IN THE FIRST SET OF ARGUMENTS
         # AFTER ADDING THE SINGLE
-        self.all[0].addText("Bob Froman", "This is the last test to test my function", Sprite("images/cutscenes/Player_PhoneCall_Right.png", 1, 7, True),
-                            "Link", "Yes it works", Sprite("images/interactive-sprites/link/link-walk-left.png", 1, 4, True))
+        self.all[0].addText("Bob Froman", "This is the last test to test my function", self.playerSprite,
+                            "Link", "Yes it works", self.linkSprite)
 
 
         # Second
         self.all[1].setTitle("Part 1: The Beginning")
         self.all[1].addText("Bob Froman",
                             "This is a test to see if the whole system works, I'm really hoping that it does",
-                            Sprite("images/cutscenes/Player_PhoneCall_Right.png", 1, 7, True),
+                            self.playerSprite,
                             "Froman", "Yes this test works well, and it is quite cool",
-                            Sprite("images/cutscenes/Wife_PhoneCall_Left.png", 1, 5, True))
+                            self.wifeSprite)
         self.all[1].addText("Bob Froman", "Thanks for your input,  Bob, it was helpful",
-                            Sprite("images/cutscenes/Player_PhoneCall_Right.png", 1, 7, True),
-                            "Froman", "Fuck off", Sprite("images/cutscenes/Wife_PhoneCall_Left.png", 1, 5, True))
+                            self.playerSprite,
+                            "Froman", "Fuck off", self.wifeSprite)
         self.all[1].addText("Bob Froman", "This is the last test to test my function",
-                            Sprite("images/cutscenes/Player_PhoneCall_Right.png", 1, 7, True))
+                            self.playerSprite)
         # IF YOU WANT THE SPEAKER TO GO FROM SINGLE TO DOUBLE, REPEAT THE TEXT IN THE FIRST SET OF ARGUMENTS
         # AFTER ADDING THE SINGLE
         self.all[1].addText("Bob Froman", "This is the last test to test my function",
-                            Sprite("images/cutscenes/Player_PhoneCall_Right.png", 1, 7, True),
+                            self.playerSprite,
                             "Link", "Yes it works",
-                            Sprite("images/interactive-sprites/link/link-walk-left.png", 1, 4, True))
+                            self.linkSprite)
 
         # Third
 
