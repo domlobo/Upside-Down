5a8dac96ab28c1e573aa65478ea40dee6eb61a25
Samuel Freeman <samfreeman8or@gmail.com>
2018-03-15 17:36:09 +0000

    Fixed centering issue of text

--- me/samfreeman/Levels/MainMenu.py
+++ me/samfreeman/Levels/MainMenu.py
@@ -10,9 +10,9 @@ class MainMenu:
         self.floor = Sprite("images/background/link/link-floor.png")
         self.dancingSprite = Sprite("images/interactive-sprites/player/Player_Dance_Right.png", 6, 9, True)
         self.title = "Game Name"
-        self.titleWidth = frame.get_canvas_textwidth(self.title, 30)
+        self.titleWidth = frame.get_canvas_textwidth(self.title, 30, "monospace")
         self.startText = "Press Space to Play"
-        self.startTextWidth = frame.get_canvas_textwidth(self.startText, 45)
+        self.startTextWidth = frame.get_canvas_textwidth(self.startText, 40, "monospace")
 
     def draw(self, canvas):
         self.background.draw(Vector((GV.CANVAS_WIDTH / 2, GV.CANVAS_HEIGHT / 2 + 40)), canvas, self.background.frameWidth, self.background.frameHeight + 150)
@@ -20,4 +20,4 @@ class MainMenu:
         self.dancingSprite.startAnimation(8, True)
         self.dancingSprite.draw(Vector((100, GV.CANVAS_HEIGHT - 150)), canvas, 200, 200)
         canvas.draw_text(self.title, ((GV.CANVAS_WIDTH - self.titleWidth) / 2, 100), 30, "White", "monospace")
-        canvas.draw_text(self.startText, ((GV.CANVAS_WIDTH - self.startTextWidth) / 2, GV.CANVAS_HEIGHT / 2), 30, "White", "monospace")
+        canvas.draw_text(self.startText, ((GV.CANVAS_WIDTH - self.startTextWidth) / 2, GV.CANVAS_HEIGHT / 2), 40, "White", "monospace")
