baeb7f08b3edbf48ed1fd22a8285855bbcf4f968
Samuel Freeman <samfreeman8or@gmail.com>
2018-03-15 17:04:46 +0000

    Updated the sprite class so that a multiple line animation can be done

--- me/samfreeman/Levels/MainMenu.py
+++ me/samfreeman/Levels/MainMenu.py
@@ -7,8 +7,12 @@ from me.samfreeman.Helper.Sprite import Sprite
 class MainMenu:
     def __init__(self):
         self.background = Sprite("")
-        self.dancingSprite = Sprite("")
+        self.dancingSprite = Sprite("images/interactive-sprites/player/Player_Dance_Right.png", 6, 9, True)
 
     def draw(self, canvas):
         # self.background.draw(Vector((GV.CANVAS_WIDTH / 2, GV.CANVAS_HEIGHT / 2)), canvas)
         canvas.draw_text("Play", (0, GV.CANVAS_HEIGHT / 2), 200,"White", "monospace")
+
+        self.dancingSprite.startAnimation(8, True)
+
+        self.dancingSprite.draw(Vector((100, 100)), canvas, 300, 300)
